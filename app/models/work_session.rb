19378bcfee5b75b7edde32e10b7324308c0972eb
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-03-11 16:19:45 +0100

    test

--- app/models/work_session.rb
+++
@@ -1,5 +0,0 @@
-class WorkSession < ApplicationRecord
-  belongs_to :goal
-  belongs_to :step
-  belongs_to :time_slot
-end
--- db/migrate/20250309213301_create_sessions.rb
+++ db/migrate/20250309213301_create_sessions.rb
@@ -1,4 +1,4 @@
-class CreateSessions < ActiveRecord::Migration[7.1]
+  class CreateSessions < ActiveRecord::Migration[7.1]
   def change
     create_table :sessions do |t|
       t.integer :day_of_week
