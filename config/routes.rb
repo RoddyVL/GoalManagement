b8b8936f70ef14e6a5ae6b544e3e51e79b12bab9
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-03-27 20:12:20 +0100

    création de session manuel

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     end
     resources :calendars, only: %i[index]
     resources :steps, only: %i[new create edit update destroy]
+    resources :sessions, only: %i[new create]
     resources :time_slots, only: %i[new create destroy index] do
       collection do
         get :generate_calendar
