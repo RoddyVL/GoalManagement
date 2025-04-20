4e72aba225600f7b07f953bad06db6219f906b4b
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-04-20 09:15:38 +0200

    création manuelle de session fonctionnelle

---
+++ app/jobs/create_session_job.rb
@@ -0,0 +1,14 @@
+class CreateSessionJob < ApplicationJob
+  queue_as :default
+
+  def perform(goal_id)
+    goal = Goal.find(goal_id)
+    session_created =  goal.sessions.order(id: :desc).first
+    puts "session created: #{session_created.start_time}"
+
+    goal.steps.where(status: 0).update_all(session_id: nil)
+    goal.sessions.where('start_time > ?', session_created.end_time).delete_all
+    puts "deleted? #{goal.sessions.where('start_time > ?', session_created.end_time).count}"
+    ReassignAfterUpdateJob.perform_now(goal_id)
+  end
+end
