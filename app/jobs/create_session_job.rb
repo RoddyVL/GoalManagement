c305f7c36d5ab1daaba3217de800e7182cfaacc6
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2025-04-23 11:51:02 +0200

    now we can create sessions

--- app/jobs/create_session_job.rb
+++ app/jobs/create_session_job.rb
@@ -1,14 +1,14 @@
 class CreateSessionJob < ApplicationJob
   queue_as :default
 
-  def perform(goal_id)
+  def perform(goal_id, session_id)
+    puts "job started"
     goal = Goal.find(goal_id)
-    session_created =  goal.sessions.order(id: :desc).first
-    puts "session created: #{session_created.start_time}"
-
-    goal.steps.where(status: 0).update_all(session_id: nil) # && goal.steps.where('session.start_time >= ?' Time.current)
-    goal.sessions.where('start_time > ?', session_created.end_time).delete_all
-    puts "deleted? #{goal.sessions.where('start_time > ?', session_created.end_time).count}"
+    session_created =  Session.find(session_id)
+    puts "session created: #{session_created.start_time} - #{session_created.end_time}"
+    goal.steps.where(session_id: Session.where('start_time > ?', session_created.start_time)).update_all(session_id: nil)
+    goal.sessions.where('start_time > ?', session_created.start_time).delete_all
+    puts "deleted? #{ goal.sessions.where('start_time > ?', session_created.start_time).size}"
     ReassignAfterUpdateJob.perform_now(goal_id)
   end
 end
